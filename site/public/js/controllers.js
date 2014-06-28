fb685e768a46cd26544e8bc34fbb9c6688dfe1a8
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2014-06-28 21:12:00 +0900

    Improve
    ・ + img-preload
    ・新規ツイートの検知ほか、リツイート数、ふぁぼ数が増加していたときだけ表示用の配列に変更を加える。

--- site/public/js/controllers.js
+++ site/public/js/controllers.js
@@ -6,30 +6,87 @@ function IndexCtrl($scope, $http) {
 }
 
 function ShowDetailCtrl($scope, $http, $routeParams) {
-  var getData = function(){
-      $http.get('/api/readAll/' + $routeParams.name).
-        success(function(data) {
-            $scope.posts = data.posts;
-            $scope.postWidth = data.postWidth;
-        });
-      $http.get('/api/readRanking/' + $routeParams.name).
-        success(function(data) {
-          $scope.rankPosts = data.rankPosts;
-          $scope.rankWidth = data.rankWidth;
-        });
-    };
-    
-  getData();
 
-  var timer = setInterval(function(){
-      getData()
+  var timer = '';
+
+  // var getData = function(){
+    $http.get('/api/readAll/' + $routeParams.name).
+      success(function(data) {
+          $scope.posts = data.posts;
+          $scope.postWidth = data.postWidth;
+      });
+    $http.get('/api/readRanking/' + $routeParams.name).
+      success(function(data) {
+        $scope.rankPosts = data.rankPosts;
+        $scope.rankWidth = data.rankWidth;
+      });
+
+  function updateTweetList(data) {
+    posts = data.rankPosts || data.posts;
+    width = data.rankWidth || data.postWidth;
+    target = data.rankPosts !== undefined ? "ranking" : "new"
+
+    posts.forEach(function(newData, newDataIndex){
+
+        if(target === "ranking") {
+          idx = _.findIndex($scope.rankPosts, {tweetId: newData.tweetId});
+        } else {
+          idx = _.findIndex($scope.posts, {tweetId: newData.tweetId});
+        }
+
+        // console.log("posts.newData", newData);
+        // console.log("idx = " + idx);
+        // console.log("newDataIndex = " + newDataIndex);
+
+        // もし、新しい画像があれば
+        if(idx === -1) {
+
+          console.log("追加します！！");
+
+          if(target === "ranking") {
+            $scope.rankPosts.push(posts[newDataIndex]);
+            $scope.rankWidth　= width;
+          } else {
+            $scope.posts.push(posts[newDataIndex]);
+            $scope.postWidth = width;
+          }
+          return;
+        }
+
+        // リツイート数、ふぁぼ数に変化があれば更新
+        if($scope.rankPosts[idx].favNum !== newData.favNum) {
+
+          // console.log("fav数を更新します！！" + $scope.rankPosts[idx].favNum + " -> " + newData.favNum);
+          if(target === "ranking") {
+            $scope.rankPosts[idx].favNum = newData.favNum;
+          } else {
+            $scope.posts[idx].favNum = newData.favNum;
+          }
+        }
+
+        if($scope.rankPosts[idx].retweetNum !== newData.retweetNum){
+
+          // console.log("retweet数を更新します！！" + $scope.rankPosts[idx].retweetNum + " -> " + newData.retweetNum);
+          if(target === "ranking") {
+            $scope.rankPosts[idx].retweetNum = newData.retweetNum;
+          } else {
+            $scope.posts[idx].retweetNum = newData.retweetNum;
+          }
+        }
+    });
+  }
+
+  timer = setInterval(function() {
+    $http.get('/api/readAll/' + $routeParams.name).
+      success(function(data) {
+        updateTweetList(data);
+      });
+    $http.get('/api/readRanking/' + $routeParams.name).
+      success(function(data) {
+
+        // console.log("data.rankPosts", data.rankPosts);
+        updateTweetList(data);
+      });
   }, 1000 * 5);
 
-}
-
-function ReadPostCtrl($scope, $http, $routeParams) {
-  $http.get('/api/post/' + $routeParams.id).
-    success(function(data) {
-      $scope.post = data.post;
-    });
 }
--- site/public/js/directives.js
+++ site/public/js/directives.js
@@ -4,17 +4,17 @@
 
 angular.module('myApp.directives', [])
   .directive('imgPreload', ['$rootScope', function($rootScope) {
-    // return {
-    //   restrict: 'A',
-    //   scope: {
-    //     ngSrc: '@'
-    //   },
-    //   link: function(scope, element, attrs) {
-    //     element.on('load', function() {
-    //       element.addClass('in');
-    //     }).on('error', function() {
-    //       //
-    //     });
-    //   }
-    // };
+    return {
+      restrict: 'A',
+      scope: {
+        ngSrc: '@'
+      },
+      link: function(scope, element, attrs) {
+        element.on('load', function() {
+          element.addClass('in');
+        }).on('error', function() {
+          //
+        });
+      }
+    };
   }]);
