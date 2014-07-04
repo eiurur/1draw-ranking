d70b3cc9e537019550692d5a2fe4d0625693c295
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2014-07-04 17:01:31 +0900

    clean

--- data/lib.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	var util    = require('util');
-	exports.cl = function(t) {
-		console.log(t);
-	};
-	exports.dump = function(obj) {
-		console.log(util.inspect(obj,false,null));
-	};
-}).call(this);
