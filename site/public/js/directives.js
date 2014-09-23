c53467cd19a3872981fc2691a3fb15e71ab143fc
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2014-09-23 13:15:01 +0900

    Refactoring

--- site/public/js/directives.js
+++ site/public/js/directives.js
@@ -55,4 +55,46 @@ angular.module('myApp.directives', [])
         });
       }
     };
-  }]);
+  }])
+  .directive('post', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        post: '='
+      },
+      template: (function () {/*
+        <img img-preload="img-preload" ng-src="{{post.sourceUrl}}" class="img-responsive thumb-pict fade"/>
+        <div class="pict-discription">
+          <div>
+            <i data-tweet-id-str="{{post.tweetIdStr}}" retweetable="retweetable" class="fa fa-retweet fa-border icon-retweet"> {{post.retweetNum}}</i>
+            <i data-tweet-id-str="{{post.tweetIdStr}}" favoritable="favoritable" class="fa fa-star fa-border icon-star"> {{post.favNum}}</i>
+            <a href="https://{{post.tweetUrl}}" target="_blank">
+              <div class="fa fa-twitter fa-border icon-twitter"></div>
+            </a>
+            <a href="{{post.sourceOrigUrl}}" download="download">
+              <div class="fa fa-download fa-border"></div>
+            </a>
+          </div>
+        </div>
+      */}).toString().replace(/(\n)/g, '').split('*')[1]
+    };
+  })
+  .directive('loading', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        judgementMaterialWhetherOrNotShowLoading: '=',
+        judgementMaterialWhetherOrNotHidePageContent: '='
+      },
+      template: (function () {/*
+        <div ng-hide="judgementMaterialWhetherOrNotShowLoading">
+          <div ng-hide="judgementMaterialWhetherOrNotHidePageContent == 0"><i class="fa fa-refresh fa-5x pull-left fa-spin"></i>
+          </div>
+          <div ng-show="judgementMaterialWhetherOrNotHidePageContent == 0"><span class="fa-stack fa-5x"><i class="fa fa-picture-o fa-stack-1x pull-left"></i><i class="fa fa-ban fa-stack-2x pull-left text-danger"></i></span>
+            <div class="fa-5x">No data ...
+            </div>
+          </div>
+        </div>
+      */}).toString().replace(/(\n)/g, '').split('*')[1]
+    };
+  });
