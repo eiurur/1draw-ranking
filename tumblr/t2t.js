0c3e60cbba360092a794acb0932f6ce667c3ae61
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2014-09-24 20:56:46 +0900

    clean

--- tumblr/t2t.js
+++ tumblr/t2t.js
@@ -22,69 +22,43 @@
       , numShow: params['numShow']
     }, function(error, postDatas) {
       if(_.isEmpty(postDatas)) return;
-      var tags, photos = [];
-      var caption = '<strong>' + (postDatas[0].tags.split(","))[2] + '</strong><hr>';
+      var tags, title;
+      var postText = '<strong>' + (postDatas[0].tags.split(","))[2] + '</strong><hr>';
       postDatas.forEach(function (postData, i) {
-
-
-        // ランキング情報を
         i++;
         if(i === 1) {
           // tags = postData.tags
           tags = (postDatas[0].tags.split(","))[3];
           title = params['correspondDate'];
-          caption += '<h2>' + i + '位</h2>';
+          postText += '<h2>' + i + '位</h2>';
         } else if (i === 2) {
-          caption += '<h3>' + i + '位</h3>';
+          postText += '<h3>' + i + '位</h3>';
         } else if (i === 3) {
-          caption += '<h4>' + i + '位</h4>';
+          postText += '<h4>' + i + '位</h4>';
         } else {
-          caption += '<h5>' + i + '位</h5>';
+          postText += '<h5>' + i + '位</h5>';
         }
-        caption += '<a href="' + postData.tweetUrl + '" target="_blank"><img src="' + postData.sourceUrl + '"></a>';
-        caption += '<blockquote>' + postData.tweetText + '</blockquote>';
-        caption += '<i class="fa fa-retweet fa-2x fa-border icon-retweet"> ' + postData.retweetNum + '</i>';
-        caption += '<i class="fa fa-star fa-2x fa-border icon-star"> ' + postData.favNum + '</i>';
-        caption += '<a href="https://twitter.com/' + postData.userId + '" target="_blank"><i class="fa fa-twitter fa-2x fa-border icon-twitter"></i></a>';
-        caption += '<br><hr><br>';
-
-
-        // 投稿する画像を配列に追加していく。ただし、ジャンル問わず10位まで。
-        if(i > 10) return;
-
-        // 投稿する画像が複数枚の場合はURLエンコードしたバイナリコンテンツでないとダメ。
-        photos.push(new Buffer(postData.sourceUrl).toString('base64'));
-        console.log("photos = ", photos);
+        postText += '<a href="' + postData.tweetUrl + '" target="_blank"><img src="' + postData.sourceUrl + '"></a>';
+        postText += '<blockquote>' + postData.tweetText + '</blockquote>';
+        postText += '<i class="fa fa-retweet fa-2x fa-border icon-retweet"> ' + postData.retweetNum + '</i>';
+        postText += '<i class="fa fa-star fa-2x fa-border icon-star"> ' + postData.favNum + '</i>';
+        postText += '<a href="https://twitter.com/' + postData.userId + '" target="_blank"><i class="fa fa-twitter fa-2x fa-border icon-twitter"></i></a>';
+        postText += '<br><hr><br>';
       });
 
       // post to tumblr
-      // my.cl("Go ------> Tumblr : " + tags);
-      // settings.tumblr.post('/post', {
-      //     type: 'text'
-      //   , tags: tags
-      //   , title: title
-      //   , body: postText
-      // }, function(err, json){
-      //   my.cl(json);
-      // });
-
-      // post to tumblr
-
       my.cl("Go ------> Tumblr : " + tags);
-      my.cl(photos);
-      // if(tags !== 'kc') return;
       settings.tumblr.post('/post', {
-          type: 'photo'
-        , link: ''
+          type: 'text'
         , tags: tags
-        , data: photos
-        , caption: caption
-      }, function(error, response){
-        if(error) console.log(error);
-        my.cl(response);
+        , title: title
+        , body: postText
+      }, function(err, json){
+        my.cl(json);
       });
     });
   };
+
 
   // for lovelive
   exports.post2Tumblr2200 = function() {
