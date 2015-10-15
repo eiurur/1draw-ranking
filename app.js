961068b326ab289032397c618c5d1d0356734212
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2015-10-16 01:28:59 +0900

    [Remove] - newRelic

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (function() {
   var async               = require('async')
-    , newrelic            = require('newrelic')
+    // , newrelic            = require('newrelic')
     , cronJob             = require('cron').CronJob
     , my                  = require('./lib/my')
     , serve               = require('./site/app').serve
