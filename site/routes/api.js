e0994c3ad9510a35d4682c93353d9f4502b9464f
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2016-12-15 05:19:59 +0900

    [Fix] - undefined is returned when fetching all posts

--- site/routes/api.js
+++ site/routes/api.js
@@ -18,6 +18,8 @@ var NUM_GET_TWEETER_TWEET = 100;
 var getPostDatas = function(params) {
   return new Promise(function(resolve, reject) {
 
+    console.log(params);
+
     PostProvider[params.query](params.opt, function(error, postDatas) {
       var postWidth = 0
         , dataCount = 0
